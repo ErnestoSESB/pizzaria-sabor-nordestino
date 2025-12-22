3760c1aa2c4a844683f90ef4a1d54dd75d70e2a0
andreff01 <andre.farias.luc@gmail.com>
2025-12-22 15:36:30 -0300

    Fix: Remove SSL redirect causando loop infinito

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,9 @@ if not DEBUG:
         'https://sabornordestino.online',
         'https://www.sabornordestino.online'
     ]
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+
+
+
 
 
 # Application definition
