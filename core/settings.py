73cd6caee1b43f46d760acdf187c7780006507cc
andreff01 <andre.farias.luc@gmail.com>
2025-12-22 16:54:28 -0300

    Fix: Adicionar collectstatic e configurar WhiteNoise

--- core/settings.py
+++ core/settings.py
@@ -146,14 +146,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # WhiteNoise para servir arquivos estáticos em produção
-# STORAGES = {
-#     "default": {
-#         "BACKEND": "django.core.files.storage.FileSystemStorage",
-#     },
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
-#     },
-# }
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
